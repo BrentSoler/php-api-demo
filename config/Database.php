5a756c5f12e8a239f56b54c91acf82e5ee4867e9
BrentSoler <brntsoler@gmail.com>
2022-04-01 00:29:05 +0800

    init

--- config/Database.php
+++ config/Database.php
@@ -1,15 +1,16 @@
 <?php
     class Database{
-        private $host = 'localhost';
-        private $dbName = 'request_api';
-        private $username ='root';
+        private $host = 'mysql8001.site4now.net';
+        private $dbName = 'db_a84ee5_reqapi';
+        private $username ='a84ee5_reqapi';
+        private $password = 'testing123';
         private $conn;
 
         public function connect(){
             $this->conn = null;
 
             try{
-                $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->dbName,$this->username);
+                $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->dbName,$this->username,$this->password);
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
             }catch(PDOException $err){
                 echo "ERROR: " . $err->getMessage(); 
